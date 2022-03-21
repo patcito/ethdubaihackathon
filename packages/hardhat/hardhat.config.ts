d455a533308e5dac8563a161ab4a6509b79732a8
Jimmy Feeder Finance <jimmy@feeder.finance>
2022-03-21 22:04:57 +0400

    feat: add coinmarketcap api for gas estimation

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -32,6 +32,7 @@ const config: HardhatUserConfig = {
     },
   },
   gasReporter: {
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
   },
