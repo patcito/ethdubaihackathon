86f657d2414510f273af88ae235db64ec141a371
Jimmy Feeder Finance <jimmy@feeder.finance>
2022-03-24 01:21:03 +0400

    feat: add ethereum hackathon contract

--- apps/web/data/constant.ts
+++ apps/web/data/constant.ts
@@ -10,7 +10,7 @@ export interface ContractAddressesInterface {
 
 export const contractAddresses: ContractAddressesInterface = {
   hackathon: {
-    1: "",
+    1: "0xC13DBa0CE613008eF43D205C455de6E9fbc1C77f",
     250: "0xcdaD183b98fC22C83895855fFfa7CF28e00E907d",
     31337: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
   },
